851d374df79d9c61cf804db67c53a05866fd301e
Salman Siddiqui <salmansiddiqui172002@gmail.com>
2025-02-22 22:17:54 +0300

    Version 0.4

---
+++ lib/features/authentication/screens/signup/signup.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_utils/src/extensions/string_extensions.dart';
+import 'package:t_store/common/widgets/login_signup/form_divider.dart';
+import 'package:t_store/common/widgets/login_signup/social_buttons.dart';
+import 'package:t_store/features/authentication/screens/signup/widgets/signup_form.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+import 'package:t_store/utils/constants/text_strings.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+          child: Padding(
+        padding: EdgeInsets.all(TSizes.defaultSpace),
+        child: Column(
+          children: [
+            // Title
+            Text(
+              TTexts.signupTitle,
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+
+            const SizedBox(height: TSizes.spaceBtwSections),
+
+            // Form
+            TSignUpForm(),
+
+            const SizedBox(height: TSizes.spaceBtwInputFields),
+
+            // Divider
+            TFormDivider(dividerText: TTexts.orSignUpWith.capitalize!),
+
+            const SizedBox(height: TSizes.spaceBtwInputFields),
+
+            // Social Buttons
+            const TSocialButtons(),
+          ],
+        ),
+      )),
+    );
+  }
+}
